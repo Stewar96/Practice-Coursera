a8745071a4761413723985656c1b0408ad03b4b2
Stewar96 <152034073+Stewar96@users.noreply.github.com>
2024-01-28 23:59:36 -0500

    Add files via upload

---
+++ Data Science Tools and Ecosystem.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# Upload the screenshot showing the markdown cell created with the title that is saved as 2-title.png,
+# as required for Exercise 2 (1 pt)
+
+
+# # DataScienceEcosystem
+
+# In[8]:
+
+
+# Upload the screenshot showing the markdown cell created with an introductory sentence that is saved as 3-intro.png, 
+# as required for Exercise 3 (1 pt)
+
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# In[17]:
+
+
+# Upload the screenshot showing the markdown cell inserted with an introductory sentence followed by an unordered 
+# list that is saved as 10-objectives.png, as required for Exercise 10. (3 pts)
+
+
+
+# **Objectives:**
+# - unordered list
+# 1. Python
+# 2. R
+# 3. SQL
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+# Upload the screenshot showing the markdown cell created with an introductory sentence followed by an ordered 
+# list of data science languages that are saved as 4-dslanguages.png, as required for Exercise 4. (3 pts)
+
+
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# Ordered list
+# 1. Python
+# 2. C++
+# 3. HTML
+# 4. ASP
+
+# In[11]:
+
+
+# Upload the screenshot showing the markdown cell created with an introductory sentence followed by an ordered -
+# list of data science libraries that are saved as 5-dslibraries.png, as required for Exercise 5. (3 pts)
+
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# Ordered list
+# 1. Pandas
+# 2. Matplotlib
+# 3. NumPy
+# 4. TensorFlow
+
+# In[12]:
+
+
+# Upload the screenshot showing the markdown cell created with a table of Data Science tools that is saved as 6-dstools.png, as required for Exercise 6. (3 pts)
+
+
+
+# |Data Science Tools|
+# |-----------|
+# |Python|
+# |R|
+# |Jupyter Notebook|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# 1. 2 + 3 = 5
+# 2. 2 - 7 = -5
+# 3. 3 * 4 = 12
+
+# In[14]:
+
+
+# Upload the screenshot showing the code cell created to multiply and add numbers that is saved as 8-multiplyandaddintegers.png, as required for Exercise 8. (2 pts)
+
+
+
+# In[13]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[16]:
+
+
+# Upload the screenshot showing the code cell created to convert minutes to hours that is saved as 9-hourstominutes.png, as required for Exercise 9. (2 pts)
+
+# This will convert 200 minutes to hours by diving by 60.
+200 / 60
+
+
+# In[18]:
+
+
+# Upload the screenshot showing the markdown cell created to indicate Author’s name that is saved as 11-authordetails.png as required for Exercise 11. (2 pts)
+
+
+# ## Author
+# STEWAR ANTHONNY CHAVEZ CLAROS
+
+# In[ ]:
+
+
+
+
